7e2ffbba8efffd088d18af59c10aa0800503e323
cjmin-n <mymemose@naver.com>
2024-08-06 23:12:37 +0900

    update react

---
+++ react-tictactoe-app/src/components/ExpenseList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./ExpenseList.css";
+
+export class ExpenseList extends Component {
+    render() {
+        return (
+            // <React.Fragment></React.Fragment> 생략 가능
+            <>
+                <ul className="list">{/* Expense Item */}</ul>
+                <button className="btn">목록 지우기</button>
+            </>
+        );
+    }
+}
+
+export default ExpenseList;
