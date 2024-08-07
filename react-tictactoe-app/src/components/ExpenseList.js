458d6f7286dcca03fd6fe585d487e126f83df8fb
cjmin-n <mymemose@naver.com>
2024-08-07 22:28:42 +0900

    ExpenseItem upload

--- react-tictactoe-app/src/components/ExpenseList.js
+++ react-tictactoe-app/src/components/ExpenseList.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
 import "./ExpenseList.css";
+import ExpenseItem from "./ExpenseItem";
 
 export class ExpenseList extends Component {
     render() {
         return (
             // <React.Fragment></React.Fragment> 생략 가능
             <>
-                <ul className="list">{/* Expense Item */}</ul>
+                <ul className="list">
+                    {
+                        /* Expense Item */
+                        <ExpenseItem />
+                    }
+                </ul>
                 <button className="btn">목록 지우기</button>
             </>
         );
